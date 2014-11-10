dddf690b658042ed009109280eef33b679256177
Joakim Holmewi <jh222qr@student.lnu.se>
2014-11-10 18:28:28 +0100

    RaknaA finished. RaknaSiffror stage one.

---
+++ RaknaA/RaknaA/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaknaA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variabler
+            string line;
+            char uppecase = 'A';
+            char lowercase = 'a';
+            int count = 0;
+
+            // Skiva en sträng
+            Console.WriteLine("Mata in en textrad:");
+            line = Console.ReadLine();
+
+            // Räkna a och A från strängen
+            // Skriv ut resultats
+            if (line.Contains(lowercase) || line.Contains(uppecase))
+            {
+                if (line.Contains(lowercase))
+                {
+                    count = line.Split(lowercase).Length - 1;
+                    Console.WriteLine("Antal {0}: {1}", lowercase, count);
+                    count = 0;
+                }
+                else if (!line.Contains(lowercase))
+                {
+                    Console.WriteLine("Inga {0}!", lowercase);
+                }
+                if (line.Contains(uppecase))
+                {
+                    count = line.Split(uppecase).Length - 1;
+                    Console.WriteLine("Antal {0}: {1}", uppecase, count);
+                    count = 0;
+                }
+                else if (!line.Contains(uppecase))
+                {
+                    Console.WriteLine("Inga {0}!", uppecase);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Det finns inga bokstäver som stämmer överens");
+            }
+
+            
+
+
+            
+
+            
+            
+
+            
+            
+        }
+    }
+}
---
+++ RaknaSiffror/RaknaSiffror/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaknaSiffror
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variabler
+            int number;
+            int count = 0;
+
+            // Skriv in ett tal
+            Console.Write("Mata in ett heltal: ");
+            number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nollor: {0}", Zero(number, count));
+
+            Console.WriteLine("Udda: {0}", OddNumbers(number, count));
+
+            Console.WriteLine("Jämna: {0}", EvenNumbers(number, count));
+            // Räkna ut antal udda siffror
+
+
+            
+        }
+        // Räkna ut antal nollor
+        static int Zero(int number, int count)
+        {
+            string myString = number.ToString();
+            count = myString.Split('0').Length - 1;
+            return count;
+        }
+
+        // Räkna ut antal udda siffror
+        // Källa: http://stackoverflow.com/questions/19482500/how-to-loop-through-an-array-thats-been-created-from-user-input-c-sharp
+        static int OddNumbers(int number, int count)
+        {
+            count = 0;
+            foreach (int i in SplitNumber(number))
+            {
+                if (i % 2 == 0 && i != 0)
+                {
+                    ++count;
+                }
+            }
+            return count;
+        }
+
+        // Räkna ut antal jämna
+        static int EvenNumbers(int number, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Delar upp talet i enskilda siffror
+        // Källa http://stackoverflow.com/questions/4808612/how-to-split-a-number-into-individual-digits
+        static int[] SplitNumber(int number)
+        {
+            List<int> list = new List<int>();
+            while(number > 0)
+            {
+                list.Add(number % 10);
+                number = number / 10;
+            }
+            list.Reverse();
+            return list.ToArray();
+           
+        }
+    }
+}
