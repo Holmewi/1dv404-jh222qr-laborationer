c9ba1dc51950c56e45db1cadb1b238d8f7f9c0c3
Joakim Holmewi <jh222qr@student.lnu.se>
2014-11-11 13:49:42 +0100

    Changed my approach with using lists due to the restrictions. Solved it with an if and if-else-statement.

--- naststorsta/NastStorsta/Program.cs
+++ naststorsta/NastStorsta/Program.cs
@@ -10,28 +10,65 @@ namespace NastStorsta
     {
         static void Main(string[] args)
         {
-            int number = 0;
-            int count = 3;
+            // Variabler
+            int count = 10;
+            int inputValue;
+            int maxValue = 0;
+            int secondMaxValue = 0;
+
+            Console.WriteLine("Mata in {0} heltal:\n", count);
+
+            // En for-loop för att mata in värdena 
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write("{0}: ", i + 1);
+                inputValue = int.Parse(Console.ReadLine());
+                
+                // Om maxvärdet är mindre än det inskrivna värdet
+                // Det näst högsta värdet är lika med maxvärdet
+                // Medan maxvärdet nu blir det nya inskrivna värdet
+
+                if (maxValue <= inputValue)
+                {
+                    secondMaxValue = maxValue;
+                    maxValue = inputValue; 
+                }
+                // Annars om det näst största värdet är mindre än det inskrivna värdet
+                // Det näst stösta värdet är lika med det inskrivna värdet
+                else if (secondMaxValue < inputValue)
+                {
+                    secondMaxValue = inputValue; 
+                }
+            }
+
+            Console.WriteLine("\nDet näst största talet är {0}", secondMaxValue);
+
+            /*
+            // Variabler
+            int count = 5;
             string input;
-            //Källa http://msdn.microsoft.com/en-us/library/9ct4ey7x(v=vs.90).aspx
+            int number;
+
+            // Skapar en tom lista
+            // Källa http://msdn.microsoft.com/en-us/library/9ct4ey7x(v=vs.90).aspx
             System.Collections.Generic.List<string> numbers = new System.Collections.Generic.List<string>();
 
             
             Console.WriteLine("Mata in {0} heltal: ", count);
 
+            // Låter användaren fylla i en tal och lägger sedan till talet i listan
             for (int i = 0; i < count; i++)
             {
                 Console.Write("{0}: ", i + 1);
-                input = Console.ReadLine();
-                numbers.Add(input);
+                numbers.Add(Console.ReadLine());
 
             }
 
-            //Numbers(number, count);
-            foreach (string ingredient in numbers)
-            {
-                System.Console.Write(ingredient);
-            }
+            // Sorterar listan och skriver ut det näst största talet
+            // Källa: http://stackoverflow.com/questions/22872592/find-the-second-max-in-a-list-of-values-using-linq-c-sharp
+            int secondMax = int.Parse(numbers.OrderByDescending(r => r).Skip(1).FirstOrDefault());
+            Console.WriteLine("\n{0}", secondMax);
+             */
         }
     }
 }
