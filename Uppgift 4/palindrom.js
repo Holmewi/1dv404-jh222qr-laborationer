0608aa4db0a37625ea7019f0791e0f2a7e583662
holmewi <jh222qr@student.lnu.se>
2014-11-24 17:13:15 +0000

    Created a new variable which contains the reversed string

--- Uppgift 4/palindrom.js
+++ Uppgift 4/palindrom.js
@@ -6,11 +6,17 @@ window.onload = function(){
         
     // Skapar en sträng och tar bort mellanrummen
     // Källa: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
-    var newString = str.toLowerCase();
-    newString = str.replace(/\s/g, '');
+    var newString = str.replace(/\s/g, '').toLowerCase();
+    
+    // Skapar en ny sträng som är omvänd
+    // Källa: http://stackoverflow.com/questions/1611427/reversing-a-string-in-javascript
+    var backString = newString.split("").reverse().join("");
+    
+
     
         console.log(newString);
-    
+        console.log(backString);
+        return newString;
     };
     
     // Referensvariabler till DOM
