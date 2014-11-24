bfbcba2605ebd9a19e7b82852880cd7c95b59d65
holmewi <jh222qr@student.lnu.se>
2014-11-24 17:35:39 +0000

    Compared the newString and the reversed string with an if-statement and returned th string. Added an eventDefault to make sure the form wont get sent. Added some palindrom examples to the html.

--- Uppgift 4/palindrom.js
+++ Uppgift 4/palindrom.js
@@ -12,17 +12,26 @@ window.onload = function(){
     // Källa: http://stackoverflow.com/questions/1611427/reversing-a-string-in-javascript
     var backString = newString.split("").reverse().join("");
     
-
+    if (newString === backString){
+        return "Normal:\t" + newString + "<br>Bakvänt:\t" + backString + "<br>\"" + str + "\" är en palindrom!";
+    } else {
+        return "Normal:\t" + newString + "<br>Bakvänt:\t" + backString + "<br>\"" + str + "\" är ingen palindrom!";
+    }
     
-        console.log(newString);
-        console.log(backString);
+        // console.log(newString);
+        // console.log(backString);
         return newString;
     };
     
     // Referensvariabler till DOM
     var input = document.querySelector("#input");
 	var submit = document.querySelector("#submit");
-    var p = document.querySelector("#value").innerHTML;
+    var p = document.querySelector("#value");
     
-    submit.addEventListener("click", function(){ p = palindrom(input.value); });
+    submit.addEventListener("click", function(evt){ 
+        // Gör att formuläret inte skickas
+        // Källa: https://developer.mozilla.org/en-US/docs/Web/API/event.preventDefault
+        evt.preventDefault();
+        p.innerHTML = palindrom(input.value); 
+    });
 };
